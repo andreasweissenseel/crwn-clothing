7f2b7475ec9e74f9ead2536b8cc2073c36e197fa
Zonk <Zonk@Gate1.home>
2021-03-04 19:19:29 +0100

    created user and cart sagas for sign in, sign up and sign in with google

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -4,8 +4,7 @@ import { connect } from 'react-redux';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import { googleSignInStart, emailSignInStart } from '../../redux/sign-in/sign-in.actions';
-//import { googleSignInStart, emailSignInStart } from '../../redux/user/user.actions';
+import { googleSignInStart, emailSignInStart } from '../../redux/user/user.actions';
 
 import {
     SignInContainer,
