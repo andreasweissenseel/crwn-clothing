7c3a2622c36fa65a6fbd05dfba7450c18b848991
Zonk <Zonk@Gate1.home>
2021-03-05 09:44:05 +0100

    remove gitignore files

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
-
-const onToken = token => {
-    console.log(token);
-    alert('Payment successful');
-}
-
+import axios from 'axios';
 const StripeCheckoutButton = ({ price }) => {
+    const currency = 'EUR'
     const priceForStripe = price * 100;
     const publishablekey = "pk_test_51EBGacIBJoahF1bcL2oNjymsf7gSBuibO3iQto1ReTRqw8AE8rO9EQvHnfBAbyqHc0J8jt884sLQsSCNHYwTDvYa00L7QEUPAV";
+
+    const onToken = token => {
+        axios({
+            url: 'payment',
+            method: 'post',
+            data: {
+                amount: priceForStripe,
+                currency,
+                token
+            }
+        }).then(response => {
+            alert('Payment successful');
+        }).catch(error => {
+            console.log('Payment error: ', JSON.parse(error));
+            alert('There was an issue with your payment. Please make sur you use the provided credit card!');
+        })
+    };
+
     return (
         <StripeCheckout
             label='Pay Now'
@@ -18,7 +32,7 @@ const StripeCheckoutButton = ({ price }) => {
             image='https://svgshare.com/i/CUz.svg'
             description={`Your total is ${price} €`}
             amount={priceForStripe}
-            currency='EUR'
+            currency={currency}
             locale='de'
             panelLabel='Pay Now'
             token={onToken}
